1cc6484d3642f21cde59f36f77a2f81630bbfe27
azizHr <azizharkati20@gmail.com>
2024-01-24 11:15:55 +0100

    adding routes in api.php file

--- url-shortener-restapi/routes/api.php
+++ url-shortener-restapi/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LongUrlController;
+use App\Http\Controllers\ShortUrlController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('long-urls', [LongUrlController::class, 'findAll'])->name('long-urls.findAll');
+Route::get('long-urls/{id}', [LongUrlController::class, 'findById'])->name('long-urls.findById');
+Route::get('most-visited', [LongUrlController::class, 'getTheMostVisited'])->name('mostVisited');
+Route::post('long-urls/create', [LongUrlController::class, 'save'])->name('long-urls.save');
+Route::post('long-urls/{id}/shorten', [ShortUrlController::class, 'shorten'])->name('long-urls.shorten');
+Route::get('{shortUrlContent}', [ShortUrlController::class, 'redirectToOriginalUrl'])->name('redirect.to.original');
