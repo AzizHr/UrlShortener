bf6e0c0ca67d0851160c582fc683e414c965804d
azizHr <azizharkati20@gmail.com>
2024-01-24 11:29:48 +0100

    add content for HomeView component

--- url-shortener-ui/src/views/HomeView.vue
+++ url-shortener-ui/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid gap-10 w-1/2 mx-auto text-center mt-32">
+    <h1 class="text-3xl font-bold uppercase">welcome to url shortener helper</h1>
+    <p class="text-xl">make your long url shorter with our service</p>
+
+    <RouterLink to="/shorten-a-url" class="bg-green-600 shadow-sm shadow-green-800 p-3 rounded-sm text-white font-bold capitalize justify-self-center">try it now</RouterLink>
+  </div>
 </template>
