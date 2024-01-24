be52d7913353e3d7f807a2d55fc1cb6f35763521
azizHr <azizharkati20@gmail.com>
2024-01-24 10:58:10 +0100

    create findAll method in controller [LongUrlController]

--- url-shortener-restapi/app/Http/Controllers/LongUrlController.php
+++ url-shortener-restapi/app/Http/Controllers/LongUrlController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\LongUrl;
 
 class LongUrlController extends Controller
 {
-    //
+    // Retrieve all long urls with its shorten urls from the database
+    public function findAll()
+    {
+        $longUrls = LongUrl::with('shortUrls')->withCount('shortUrls')->get();
+
+        if ($longUrls->isEmpty()) {
+            return response()->json(['message' => 'No LongUrls found']);
+        }
+
+        return response()->json($longUrls);
+    }
 }
