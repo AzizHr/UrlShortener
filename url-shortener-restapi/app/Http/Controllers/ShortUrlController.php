11681716b64cfcdf19ff9336cc61342dce340583
azizHr <azizharkati20@gmail.com>
2024-01-24 11:02:27 +0100

    create shorten method in controller [ShortUrlController]

--- url-shortener-restapi/app/Http/Controllers/ShortUrlController.php
+++ url-shortener-restapi/app/Http/Controllers/ShortUrlController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShortUrl;
+use App\Services\RandomStringGeneratorService;
 use Illuminate\Http\Request;
 
 class ShortUrlController extends Controller
 {
-    //
+    
+    public function __construct(
+        protected LongUrlController $longUrlController,
+        protected RandomStringGeneratorService $randomStringGeneratorService
+    ) {
+    }
+
+    // Save the short url in the database
+    public function shorten($longUrlId)
+    {
+        try {
+            $longUrl = $this->longUrlController->findById($longUrlId);
+
+            $shortUrl = new ShortUrl([
+                'content' => $this->randomStringGeneratorService->generate(),
+                'long_url_id' => $longUrl->id
+            ]);
+
+            $shortUrl->save();
+
+            return response()->json(['short_url' => $shortUrl->content]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 404);
+        }
+    }
 }
