e23574888d8788d7d4510f8938dc17a2aca61b7d
azizHr <azizharkati20@gmail.com>
2024-01-24 10:40:28 +0100

    add content for model [LongUrl]

--- url-shortener-restapi/app/Models/LongUrl.php
+++ url-shortener-restapi/app/Models/LongUrl.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LongUrl extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'content'
+    ];
+
+    public function shortUrls(): HasMany
+    {
+        return $this->hasMany(ShortUrl::class);
+    }
 }
