ddbd44e96e109616cad06398182607a6dd200cc5
azizHr <azizharkati20@gmail.com>
2024-01-24 10:36:51 +0100

    add schema properties for migration [short_urls]

--- url-shortener-restapi/database/migrations/2024_01_24_093343_short_urls.php
+++ url-shortener-restapi/database/migrations/2024_01_24_093343_short_urls.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('short_urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->integer('clicks')->default(0);
+            $table->unsignedBigInteger('long_url_id'); 
+            $table->foreign('long_url_id')->references('id')
+            ->on('long_urls')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('short_urls');
     }
 };
