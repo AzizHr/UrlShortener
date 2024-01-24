088e3383df3e17ca3bb20941e55bd4fc4c01ef6a
azizHr <azizharkati20@gmail.com>
2024-01-24 11:04:12 +0100

    create redirectToOriginalUrl method in controller [ShortUrlController]

--- url-shortener-restapi/app/Http/Controllers/ShortUrlController.php
+++ url-shortener-restapi/app/Http/Controllers/ShortUrlController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ShortUrl;
 use App\Services\RandomStringGeneratorService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class ShortUrlController extends Controller
 {
@@ -41,6 +42,24 @@ class ShortUrlController extends Controller
             return ShortUrl::where('content', $content)->firstOrFail();
         } catch (\Exception $e) {
             throw new \Exception('No ShortUrl was found with the provided content');
+        }
+    }
+
+    // Redirect to the original url if the short one has been clicked
+    public function redirectToOriginalUrl($shortUrlContent)
+    {
+        try {
+            $shortUrl = $this->findByContent($shortUrlContent);
+            $longUrl = $this->longUrlController->findById($shortUrl->long_url_id);
+
+            if ($longUrl) {
+                $this->incrementClicks($shortUrl);
+                return Redirect::to($longUrl->content);
+            }
+
+            abort(404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 404);
         }
     }
 }
