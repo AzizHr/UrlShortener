3c71220049a1968b94c099f763dd4473a890b191
azizHr <azizharkati20@gmail.com>
2024-01-24 10:41:07 +0100

    add content for model [ShortUrl]

--- url-shortener-restapi/app/Models/ShortUrl.php
+++ url-shortener-restapi/app/Models/ShortUrl.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ShortUrl extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'long_url_id'
+    ];
+
+    public function longUrl(): BelongsTo
+    {
+        return $this->belongsTo(LongUrl::class);
+    }
 }
