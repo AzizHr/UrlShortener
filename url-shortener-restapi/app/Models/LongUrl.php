48f0ecd224c9eba51be1b69bcdc1ef8a330e1d78
azizHr <azizharkati20@gmail.com>
2024-01-24 10:38:44 +0100

    create model [LongUrl]

---
+++ url-shortener-restapi/app/Models/LongUrl.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LongUrl extends Model
+{
+    use HasFactory;
+}
