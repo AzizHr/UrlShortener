b38cfb78ea4e436fe2ac8d7b0dcca310393e0bda
azizHr <azizharkati20@gmail.com>
2024-01-24 11:31:51 +0100

    add content for Navbar component

---
+++ url-shortener-ui/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <nav
+    class="flex justify-center items-center justify-between px-16 py-6 shadow-sm shadow-green-400"
+  >
+    <RouterLink to="/" class="font-bold">Url-Shortener</RouterLink>
+    <ul class="flex gap-6">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/shorten-a-url">Shorten a URL</RouterLink>
+      <RouterLink to="/urls-stats">Urls Statistics</RouterLink>
+    </ul>
+  </nav>
+</template>
