48e8e0e9154b9fb0e890d942e318a556558f5a84
azizHr <azizharkati20@gmail.com>
2024-01-24 10:39:13 +0100

    create model [ShortUrl]

---
+++ url-shortener-restapi/app/Models/ShortUrl.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShortUrl extends Model
+{
+    use HasFactory;
+}
